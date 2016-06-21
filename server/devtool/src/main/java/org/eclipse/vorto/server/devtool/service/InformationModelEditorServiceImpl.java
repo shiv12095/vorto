6c7cba7dc98ac645bb7bf411d0366028268827e2
shiv12095 <shiv12095@iiitd.ac.in>
2016-06-21 14:52:00 +0530

    Added generation of variable names on Function Block addition
    Signed-off-by: shiv12095 <shiv12095@iiitd.ac.in>

--- server/devtool/src/main/java/org/eclipse/vorto/server/devtool/service/InformationModelEditorServiceImpl.java
+++ server/devtool/src/main/java/org/eclipse/vorto/server/devtool/service/InformationModelEditorServiceImpl.java
@@ -5,19 +5,23 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
+import java.util.Set;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.vorto.core.api.model.functionblock.FunctionblockModel;
+import org.eclipse.vorto.core.api.model.informationmodel.FunctionblockProperty;
 import org.eclipse.vorto.core.api.model.informationmodel.InformationModel;
+import org.eclipse.vorto.core.api.model.informationmodel.InformationModelFactory;
 import org.eclipse.vorto.core.api.model.model.Model;
 import org.eclipse.vorto.core.api.model.model.ModelId;
 import org.eclipse.vorto.core.api.model.model.ModelReference;
 import org.eclipse.vorto.core.api.model.model.ModelType;
 import org.eclipse.vorto.editor.infomodel.web.resource.InformationModelResourceSetProvider;
 import org.eclipse.vorto.server.devtool.utils.InformationModelEditorUtils;
-import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.web.server.model.IWebResourceSetProvider;
 import org.eclipse.xtext.web.servlet.HttpServiceContext;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -74,9 +78,10 @@ public class InformationModelEditorServiceImpl implements IInformationModelEdito
 					.asModelReference();
 
 			URI uri = URI.createURI("fake:/" + fileName);
+			Resource resource = null;
+			EObject eObject = null;
 
 			if (!containsModelReference(informationModel, modelReference)) {
-				Resource resource = null;
 				if (!referencedResourceSet.contains(fileName)) {
 					String fileContents = informationModelEditorUtils.downloadModelFile(namespace, name, version);
 					try {
@@ -87,19 +92,22 @@ public class InformationModelEditorServiceImpl implements IInformationModelEdito
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
-				} else {
-					resource = resourceSet.getResource(uri, true);
 				}
-				EObject eObject = resource.getContents().get(0);
+				resource = resourceSet.getResource(uri, true);
+				eObject = resource.getContents().get(0);
 				targetResource.getContents().add(eObject);
 				informationModel.getReferences().add(modelReference);
-				EcoreUtil2.resolveAll(resourceSet);
+				resource.getContents().add(eObject);
 			}
+			resource = resourceSet.getResource(uri, true);
+			eObject = resource.getContents().get(0);
+			FunctionblockModel funtionblockModel = (FunctionblockModel) eObject;
+			informationModel.getProperties().add(
+					createFunctionblockProperty(funtionblockModel, getVariableNames(informationModel.getProperties())));
+			resource.getContents().add(eObject);
 			try {
 				ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 				targetResource.save(byteArrayOutputStream, null);
-				System.out.println("The resourceId is : " + resourceId);
-				System.out.println(byteArrayOutputStream.toString());
 				return byteArrayOutputStream.toString();
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -117,35 +125,30 @@ public class InformationModelEditorServiceImpl implements IInformationModelEdito
 		return false;
 	}
 
-	// private Set<String> getVariableNames(EList<FunctionblocSkProperty>
-	// properties) {
-	// Set<String> variableNames = new HashSet<String>();
-	// for (FunctionblockProperty property : properties) {
-	// variableNames.add(property.getName());
-	// }
-	// return variableNames;
-	// }
-	//
-	// private FunctionblockProperty
-	// createFunctionblockProperty(FunctionblockModel functionblockModel,
-	// Set<String> existingVariableNames) {
-	// FunctionblockProperty functionblockProperty =
-	// InformationModelFactory.eINSTANCE.createFunctionblockProperty();
-	// functionblockProperty.setType(functionblockModel);
-	// functionblockProperty.setName(generateFunctionBlockVariableName(functionblockModel,
-	// existingVariableNames));
-	// return functionblockProperty;
-	// }
-	//
-	// private String generateFunctionBlockVariableName(FunctionblockModel fbm,
-	// Set<String> variableNames) {
-	// String variableName = fbm.getName().toLowerCase();
-	// int i = 0;
-	// while (variableNames.contains(variableName)) {
-	// variableName += ++i;
-	// }
-	// return variableName;
-	// }
+	private Set<String> getVariableNames(EList<FunctionblockProperty> properties) {
+		Set<String> variableNames = new HashSet<String>();
+		for (FunctionblockProperty property : properties) {
+			variableNames.add(property.getName());
+		}
+		return variableNames;
+	}
+
+	private FunctionblockProperty createFunctionblockProperty(FunctionblockModel functionblockModel,
+			Set<String> existingVariableNames) {
+		FunctionblockProperty functionblockProperty = InformationModelFactory.eINSTANCE.createFunctionblockProperty();
+		functionblockProperty.setType(functionblockModel);
+		functionblockProperty.setName(generateFunctionBlockVariableName(functionblockModel, existingVariableNames));
+		return functionblockProperty;
+	}
+
+	private String generateFunctionBlockVariableName(FunctionblockModel fbm, Set<String> variableNames) {
+		String variableName = fbm.getName().toLowerCase();
+		int i = 0;
+		while (variableNames.contains(variableName)) {
+			variableName += ++i;
+		}
+		return variableName;
+	}
 
 	private String createFileName(String namespace, String name, String version, ModelType modelType) {
 		return namespace.replace(".", "_") + "_" + name + "_" + version.replace(".", "_") + modelType.getExtension();
